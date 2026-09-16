7895209d030845dc5f1d86495916f14915db0b7b
louis99941 <louis99941@yahoo.com.tw>
2026-09-16 22:32:26 +0800

    Register additional reverse image search engines

--- src/engines/registry.js
+++ src/engines/registry.js
@@ -6,8 +6,19 @@ import { sogou } from './sogou.js';
 import { baidu } from './baidu.js';
 import { lenso } from './lenso.js';
 import { copyseeker } from './copyseeker.js';
+import { saucenao } from './saucenao.js';
+import { iqdb } from './iqdb.js';
+import { ascii2d } from './ascii2d.js';
+import { tracemoe } from './tracemoe.js';
+import { animetrace } from './animetrace.js';
+import { pinterest } from './pinterest.js';
+import { pimeyes } from './pimeyes.js';
+import { shutterstock } from './shutterstock.js';
 
-export const engines = [google, bing, yandex, tineye, sogou, baidu, lenso, copyseeker];
+export const engines = [
+  google, bing, yandex, tineye, sogou, baidu, lenso, copyseeker,
+  saucenao, iqdb, ascii2d, tracemoe, animetrace, pinterest, pimeyes, shutterstock
+];
 
 export function selectedEngines(ids) {
   return engines.filter((engine) => ids.includes(engine.id));
