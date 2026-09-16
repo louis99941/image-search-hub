90a5c61495a0216fa5a2ca3e8aef41ded1fda43c
louis99941 <louis99941@yahoo.com.tw>
2026-09-16 18:55:35 +0800

    feat: register Baidu and Sogou engines

--- src/engines/registry.js
+++ src/engines/registry.js
@@ -2,10 +2,12 @@ import { google } from './google.js';
 import { bing } from './bing.js';
 import { yandex } from './yandex.js';
 import { tineye } from './tineye.js';
+import { sogou } from './sogou.js';
+import { baidu } from './baidu.js';
 import { lenso } from './lenso.js';
 import { copyseeker } from './copyseeker.js';
 
-export const engines = [google, bing, yandex, tineye, lenso, copyseeker];
+export const engines = [google, bing, yandex, tineye, sogou, baidu, lenso, copyseeker];
 
 export function selectedEngines(ids) {
   return engines.filter((engine) => ids.includes(engine.id));
