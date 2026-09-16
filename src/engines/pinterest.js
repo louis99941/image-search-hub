9bbdbb0c0425401318763906834e6223de72c34f
louis99941 <louis99941@yahoo.com.tw>
2026-09-16 22:32:07 +0800

    Add Pinterest Lens image search engine

---
+++ src/engines/pinterest.js
@@ -0,0 +1,6 @@
+export const pinterest = {
+  id:'pinterest', name:'Pinterest Lens', badge:'視覺',
+  description:'Pinterest 視覺搜尋；可在官方網站上傳圖片尋找相似靈感與商品。', mode:'manual',
+  buildUrl(){ return 'https://www.pinterest.com/'; },
+  buildManualUrl(){ return 'https://www.pinterest.com/'; }
+};
